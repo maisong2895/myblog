2c8dd06df72b0ffe1bf35c2bfd96f92203384d3a
Xuesong Shang <xuesongshang@Xuesongs-MBA.local>
2020-03-01 21:35:10 -0500

    first commit

---
+++ src/build/precache-manifest.5201b8f74f790900b7b2ac2afc3741ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5d9b2c9b5fc5988ed4bff4e3f4eb2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98f4f619cd0adef18148",
+    "url": "/static/css/main.b20b2dbc.chunk.css"
+  },
+  {
+    "revision": "2625b3d5b32a229e2485",
+    "url": "/static/js/2.c0674058.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c0674058.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98f4f619cd0adef18148",
+    "url": "/static/js/main.aeaa2408.chunk.js"
+  },
+  {
+    "revision": "609d04954aaf9eb8196b",
+    "url": "/static/js/runtime-main.7237aa21.js"
+  }
+]);
